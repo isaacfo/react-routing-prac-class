6415ce621a416e6f68b9efa67890764a3b442023
Isaac Fonseca <isaacfo1010@gmail.com>
2018-12-10 11:27:30 -0500

    added cat names as subcomponents of cats.js within the file

--- src/Cats.js
+++ src/Cats.js
@@ -1,14 +1,24 @@
 import React from "react"
+// grabbing individual link component from that library
+import {Link} from "react-router-dom"
 
 
 const Cats = (props) => {
     return (
         <div>
             <h1>Catssss!</h1>
-
+            <ul>
+                <li>
+                  <Link to ="/cats/oakley">Oakley</Link>
+                </li>
+                <li>
+                  <Link to="/cats/milla">Milla</Link>
+                </li>
+          </ul>
+        
         </div>
-    )
-}
+    );
+};
 
 
 export default Cats;
--- src/NavBar.js
+++ src/NavBar.js
@@ -8,17 +8,10 @@ const NavBar = (props)  => {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/cats">Cats</Link></li>
-          <ul>
-              <li>
-                  <Link to ="/cats/oakley">Oakley</Link>
-              </li>
-              <li>
-                  <Link to="/cats/milla">Milla</Link>
-              </li>
           </ul>
-        </ul>
-    )
-}
+             
+    );
+};
 
 
 
